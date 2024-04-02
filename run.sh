6f96fe2c77fd9a3f999f96ebf714e08c146aefea
Gustavo Michels de Camargo <guga.power@hotmail.com>
2024-04-02 10:33:04 -0300

    Verificando a instalação do meson

--- run.sh
+++ run.sh
@@ -1,5 +1,15 @@
 #! /bin/bash
 
+# Check if meson is installed
+if ! pip3 show meson &> /dev/null; then
+    echo "meson is not installed. Installing now..."
+    pip3 install meson
+    # Reload bash source
+    echo "Reloading bash source..."
+    source ~/.bashrc
+fi
+
+
 NOME_PROJETO="main" # Nome do projeto
 
 # Configurações
